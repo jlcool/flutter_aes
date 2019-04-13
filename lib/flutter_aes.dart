180dc9ff92e4b728847c43c4db6ed0d7cb3cf5b0
jiangliang <jiangliang.net@gmail.com>
2019-04-13 11:08:40 +0800

    添加描述

--- lib/flutter_aes.dart
+++ lib/flutter_aes.dart
@@ -5,19 +5,14 @@ import 'package:flutter/services.dart';
 
 class FlutterAes {
   static const MethodChannel _channel = const MethodChannel('flutter_aes');
-
+///解密
   static Future<Uint8List> decrypt(
       Uint8List data, Uint8List key, Uint8List iv) async {
     final Uint8List result = await _channel
         .invokeMethod('decrypt', {"data": data, "key": key, "iv": iv});
     return result;
   }
-  static Future<String> decryptString(
-      String data, String key, String iv) async {
-    final String result = await _channel
-        .invokeMethod('decryptString', {"dataString": data, "keyString": key, "iv": iv});
-    return result;
-  }
+///加密
   static Future<Uint8List> encrypt(
       Uint8List data, Uint8List key, Uint8List iv) async {
     final Uint8List result = await _channel
